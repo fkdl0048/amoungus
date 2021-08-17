1c6a58d9454e764c211fb13f3944e2af374a66e4
이정안 <84510455+fkdl0048@users.noreply.github.com>
2021-08-17 17:27:07 +0900

    어몽어스 초기 UI설정

---
+++ Assets/UI/Scripts/SettingsUI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button MouseControlButton;
+    [SerializeField]
+    private Button KeyboardMouseContorlButton;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        switch (PlayerSettings.controlType)
+        {
+            case EControlType.Mouse:
+                MouseControlButton.image.color = Color.green;
+                KeyboardMouseContorlButton.image.color = Color.white;
+                break;
+
+            case EControlType.KeyboardMouse:
+                MouseControlButton.image.color = Color.white;
+                KeyboardMouseContorlButton.image.color = Color.green;
+                break;
+        }
+    }
+
+    public void SetControlMode(int controlType)
+    {
+        PlayerSettings.controlType = (EControlType)controlType;
+        switch (PlayerSettings.controlType)
+        {
+            case EControlType.Mouse:
+                MouseControlButton.image.color = Color.green;
+                KeyboardMouseContorlButton.image.color = Color.white;
+                break;
+
+            case EControlType.KeyboardMouse:
+                MouseControlButton.image.color = Color.white;
+                KeyboardMouseContorlButton.image.color = Color.green;
+                break;
+        }
+    }
+
+    public void Close()
+    {
+        StartCoroutine(CloseAfterDelay());
+    }
+
+    private IEnumerator CloseAfterDelay()
+    {
+        animator.SetTrigger("Close");
+        yield return new WaitForSeconds(0.5f
+            );
+        gameObject.SetActive(false);
+        animator.ResetTrigger("Close");
+    }
+
+}

39c077052be48d3072e7ac90680fd6923da8a233
이정안 <84510455+fkdl0048@users.noreply.github.com>
2021-09-07 00:01:05 +0900

    대기실 및 레이어 수정

--- Assets/Scripts/AmongUsRoomManager.cs
+++ Assets/Scripts/AmongUsRoomManager.cs
@@ -5,5 +5,13 @@ using Mirror;
 
 public class AmongUsRoomManager : NetworkRoomManager
 {
+    public override void OnRoomServerConnect(NetworkConnection conn)
+    {
+        base.OnRoomServerConnect(conn);
 
+        Vector3 spawnPos = FindObjectOfType<SpawnPositions>().GetSpawnPosition();
+
+        var player = Instantiate(spawnPrefabs[0],spawnPos,Quaternion.identity);
+        NetworkServer.Spawn(player, conn);
+    }
 }
